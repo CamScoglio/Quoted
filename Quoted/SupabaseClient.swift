3d5a056487a41159d76f30b2dba8af4a685fe706
CamScoglio <cameron.scoggs@gmail.com>
2025-06-25 23:09:27 -0400

    fixed config

--- Quoted/SupabaseClient.swift
+++ Quoted/SupabaseClient.swift
@@ -15,8 +15,8 @@ class SupabaseManager: ObservableObject {
     private init() {
         guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
               let config = NSDictionary(contentsOfFile: path),
-              let urlString = config["https://dsrznnimciulnsnrqxoq.supabase.co"] as? String,
-              let key = config["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzcnpubmltY2l1bG5zbnJxeG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2MzYyNDUsImV4cCI6MjA2NjIxMjI0NX0.IN2PXEz2ZSqw2DO753-2PLwy_Jf9Q5BcgY067xpiwDg"] as? String,
+              let urlString = config["SUPABASE_URL"] as? String,
+              let key = config["SUPABASE_ANON_KEY"] as? String,
               let url = URL(string: urlString) else {
             fatalError("Could not load Supabase configuration")
         }
